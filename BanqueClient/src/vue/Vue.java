1b08c4ae80084dc1f72103e959b3ba1537ffecaa
Thodei <45964385+Thodei@users.noreply.github.com>
2019-05-04 00:22:51 +0200

    Add files via upload

--- BanqueClient/src/vue/Vue.java
+++ BanqueClient/src/vue/Vue.java
@@ -13,19 +13,17 @@ public class Vue {
 	public static Stage parentWindow;
 	public static String NumCompte;
 	
-	public Vue(String VueName,Stage arg0,String NumCompte) throws Exception {
+	public Vue(String VueName,Stage arg0,String NumCompte) throws Exception 
+	{
 		if ( arg0 != null )
 			parentWindow = arg0;	
 		this.NumCompte = NumCompte;
 		parentWindow.setTitle(VueName+" - BanqueClient");		
-		
-		final URL fxml = getClass().getResource("/Vue/"+VueName+".fxml");
+		final URL fxml = getClass().getResource("/vue/"+VueName+".fxml");
 		final FXMLLoader fxmlLoader = new FXMLLoader(fxml);
 		final VBox nodeParent = (VBox) fxmlLoader.load();
-		
 		Scene scene = new Scene(nodeParent);
 		Main.parentWindow.setScene(scene);
-		
 		Main.parentWindow.show();
 	}
 }
